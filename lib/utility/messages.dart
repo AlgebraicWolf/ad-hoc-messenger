83ad79be77d61a81a9f09cd92eb6b2fa03b87e95
Aleksei Volkov <volkov.aa@phystech.edu>
2021-04-29 22:13:09 +0300

    First approximation of message classes

---
+++ lib/utility/messages.dart
@@ -0,0 +1,38 @@
+// Higher-level message type that is used for the purposes of rendering and
+// database interaction
+class ChatMessage {
+  String otherHandle; // Handle of a friend
+  bool mine; // Whether the message is mine
+  String text; // Contents of a message
+  DateTime sentAt; // Time of message reception
+}
+
+// Lower-level network representationg of a message object
+class NetworkMessage {
+  String handle; // Handle of a message source
+  int randomId; // Random ID designed for message duplicate avoidance
+  String text; // Message contents (will be encoded in)
+  DateTime sentAt; // Time of message creation
+
+  // TODO Maybe insert time for rendering as well
+  NetworkMessage(this.handle, this.randomId, this.text, sentAt);
+
+  NetworkMessage.fromChatMessage(ChatMessage msg, int randomId, String handle)
+      : handle = handle,
+        randomId = randomId,
+        text = msg.text,
+        sentAt = msg.sentAt;
+
+  NetworkMessage.fromJson(Map<String, dynamic> json)
+      : handle = json['handle'],
+        randomId = json['randomId'],
+        text = json['text'],
+        sentAt = json['sentAt'];
+
+  Map<String, dynamic> toJson() => {
+        'handle': handle,
+        'randomId': randomId,
+        'text': text,
+        'sentAt': sentAt,
+      };
+}
